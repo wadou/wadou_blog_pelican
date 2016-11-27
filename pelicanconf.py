afc0717017ec95cb45627fe049357b995db015d9
wadou <wei_pengfei@163.com>
2016-11-27 17:09:02 +0800

    add themes "pelican-elegant" and plugins "extract_toc"

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "themes/pelican-elegant"
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["extract_toc"]
